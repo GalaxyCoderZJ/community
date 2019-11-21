5452c3d759db520def3001368b2e4ff855a30a29
GalaxyCoderZJ <610787215@qq.com>
2019-11-21 18:43:57 +0800

    first commit

---
+++ src/test/java/com/zj/community/demo/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zj.community.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
